edfcb3bab56e31682553017f8abe449e1232e5b0
Leo Martin <contact@leomartin.ch>
2025-04-01 14:31:34 +0200

    Add comments and lerp utility function

--- vec3.h
+++ vec3.h
@@ -56,44 +56,51 @@ inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
-inline vec3 operator+(const vec3& u, const vec3& v) {
+inline vec3 operator+(const vec3& u, const vec3& v) { // two vector addition
     return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
-inline vec3 operator-(const vec3& u, const vec3& v) {
+inline vec3 operator-(const vec3& u, const vec3& v) { // two vector subtraction
     return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
-inline vec3 operator*(const vec3& u, const vec3& v) {
+inline vec3 operator*(const vec3& u, const vec3& v) { // two vector multiplication
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator*(double t, const vec3& v) {
+inline vec3 operator*(double t, const vec3& v) { // vector scalar multiplication
     return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
-inline vec3 operator*(const vec3& v, double t) {
+inline vec3 operator*(const vec3& v, double t) { // vector scalar multiplication
     return t * v;
 }
 
-inline vec3 operator/(const vec3& v, double t) {
+inline vec3 operator/(const vec3& v, double t) { // vector division
     return (1/t) * v;
 }
 
-inline double dot(const vec3& u, const vec3& v) {
+inline double dot(const vec3& u, const vec3& v) { // dot product (skalarprodukt)
     return u.e[0] * v.e[0]
          + u.e[1] * v.e[1]
          + u.e[2] * v.e[2];
 }
 
-inline vec3 cross(const vec3& u, const vec3& v) {
+inline vec3 cross(const vec3& u, const vec3& v) { // cross product (vektorprodukt
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
                 u.e[2] * v.e[0] - u.e[0] * v.e[2],
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline vec3 unit_vector(const vec3& v) {
+inline vec3 normalize(const vec3& v) { // einheitsvektor (vector has length 1)
     return v / v.length();
 }
+
+inline vec3 lerp(const vec3& v1, const vec3& v2, double factor) { // blend between two
+	return vec3(v1.e[0] * (1 - factor) + v2.e[0] * factor,
+			    v1.e[1] * (1 - factor) + v2.e[1] * factor,
+			    v1.e[2] * (1 - factor) + v2.e[2] * factor);
+}
+
 
 #endif
