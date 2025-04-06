f36d72b17d3f9bc98bd8af6c424e4fe3de59e956
Leo Martin <contact@leomartin.ch>
2025-04-06 10:17:36 +0200

    Updated the main.cc script

--- main.cc
+++ main.cc
@@ -1,74 +1,20 @@
-//#include "vec3.h"
-//#include "color.h"
-//#include "ray.h"
-//#include "rtweekend.h"
-#include "leoraytracer.h"
+#include "leo-raytracer.h"
+#include "mesh_scene.h"
+#include "mesh.h"
+#include "ray.h"
+#include "color.h"
 
-#include "hittable.h"
-#include "hittable_list.h"
-
+#include <memory>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <vector>
 #include <iostream>
 using namespace std;
 
 // RAY-TRACER //
 // Leo Martin (2025) //
-
-
-bool get_ray_mesh_intersection(ray render_ray, point3 vertices[3]) {
-    vec3 edge_1 = vertices[1] - vertices[0];
-    vec3 edge_2 = vertices[2] - vertices[0];
-
-    vec3 p_vector = cross(render_ray.direction(), edge_2);
-    double determinant = dot(edge_1, p_vector);
-
-    const float kepsilon = 1e-8f;
-    if (fabs(determinant) < kepsilon) {
-        return false; // ray is parallel or invalid
-    }
-
-    float inverse_determinant = 1 / determinant;
-    vec3 ray_to_vertice_0 = render_ray.origin() - vertices[0];
-
-    double u = dot(ray_to_vertice_0, p_vector) * inverse_determinant;
-    if (u < 0 || u > 1) { // ...explanation
-        return false;
-    }
-
-    vec3 q = cross(ray_to_vertice_0, edge_1);
-    double v = dot(render_ray.direction(), q) * inverse_determinant;
-    if (v < 0 || (u + v) > 1) { // ...explanation
-        return false;
-	}
-
-    double t = dot(edge_2, q) * inverse_determinant;
-    if (t > 0){
-        //intersection_point = add(ray_origin, multiply_scalar(ray_direction, t))
-        return true;
-    }
-
-    return false; // ray intersects triangle behind camera
-}
-
-/*
-// returns the final color of the path
-color trace_path(ray render_ray, int samples, int max_bounces) {
-    color final_color = color(0,0,0);
-
-	for (int i = 0; i < samples; i++) {
-			color throughput = color(1,1,1);
-
-			for (int j = 0; j < max_bounces; j++) {
-				point3 intersection_point = get_object_hit(render_ray, 
-
-				throughput = throughput * object.color;
-				throughput = multiply scalar
-			}
-		
-	}
-}
-
-*/
-
 
 
 // render image
@@ -77,7 +23,7 @@ int main() {
 	const int image_height = 256;
 	
 	const float start_x = -2;
-	const float start_y = 1.5;
+	const float start_y = 2;
 
     const float pixel_size = -2 * start_x / image_width;
 
@@ -85,38 +31,47 @@ int main() {
 	const int height = image_height / pixel_size;
     const int width = image_width / pixel_size;
 
-    point3 vertices[3] = {point3(-2.0, -1.5, -3.0),point3(0.0, 2.0, -3.0),point3(2.0, -1.5, -3.0)}; // mesh triangle
+	// initiate scene (populate with meshes)
+	MeshScene scene;
 
-	float x_pos, y_pos;
-	// render	
-	cout << "P3\n" << image_width << ' ' << image_height << "\n255\n"; // PPM header
+	scene.add(make_shared<Mesh>("objects/icosphere.obj"));
+    scene.add(make_shared<Mesh>("objects/cone.obj"));
+    //scene.add(make_shared<Mesh>("objects/cone2.obj"));
+	//scene.add(make_shared<Mesh>("objects/cone3.obj"));
+	//scene.add(make_shared<Mesh>("objects/cone4.obj"));
+	
+	// render
+	cout << "P3\n" << image_width << ' ' << image_height << "\n255\n"; // PPM header 
     color pixel_color;
-	for (int j = 0; j < image_height; j++) {
-		clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+    float x_pos, y_pos;
+
+	for (int j = 0; j < image_height; j++) { // row
+		clog << "\rScanlines remaining: " << (image_height - j) << ' ' << flush; // progress meter
 		y_pos = start_y - (pixel_size * j) - center_pixel;
-		for (int i = 0; i < image_height; i++) {
+		for (int i = 0; i < image_width; i++) { // column
 		    x_pos = start_x + (pixel_size * i) + center_pixel;
 			
-			vec3 cell_center(x_pos,y_pos,0);
+			vec3 cell_center(x_pos,y_pos,6); // cell start has to not clip through the object
 			vec3 ray_direction(0, 0, -1);
 
 		    ray_direction = normalize(ray_direction);
             ray render_ray(cell_center, ray_direction);
 
-		    bool hit = get_ray_mesh_intersection(render_ray, vertices);
-
-		    
-			if (hit) {
-				    pixel_color = color(1, 0, 0);
-			} else {
-					pixel_color = color(0, 0, 0);
-			}	
-
-            write_color(std::cout, pixel_color);
-		}
-    }
+		    pixel_color = color(0,0,0);
+			RayHit hit = scene.hit(render_ray);
 	
-	clog << "\rDone.                            \n";
+			if (hit.hit_time > 0.0) {	// if there is an intersection with a triangle
+				point3 intersection_point = render_ray.at(hit.hit_time);
+				vec3 normal_vec = dynamic_cast<Mesh*>(hit.hit_object)->get_normal_vector(hit.face_id, render_ray);
+
+				vec3 mapped_normal = 0.5 * (normal_vec + vec3(1,1,1)); // for rgb coloring
+				pixel_color = color(mapped_normal[0], mapped_normal[1], mapped_normal[2]);	
+		    }
+		    write_color(cout, pixel_color);
+		}
+
+    }
+	clog << "\rRender Done                            \n";
 }
 				
 
