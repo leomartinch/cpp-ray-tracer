58e1d4be38093e041f0f52f9fad89e020e59080a
Leo Martin <contact@leomartin.ch>
2025-04-13 20:58:23 +0200

    Clamp color components for accurate colors

--- color.h
+++ color.h
@@ -2,7 +2,6 @@
 #define COLOR_H
 
 #include "vec3.h"
-
 #include <iostream>
 
 using color = vec3;
@@ -12,13 +11,18 @@ inline void write_color(std::ostream& out, const color& pixel_color) {
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
-    // Translate the [0,1] component values to the byte range [0,255].
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+	// clamp each color component to the range [0, 1]
+    r = std::min(r, 1.0);
+    g = std::min(g, 1.0);
+    b = std::min(b, 1.0);
 
-    // Write out the pixel color components.
-    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+    // translate the [0,1] component values to the byte range [0,255]
+    int red_byte = int(255.999 * r);
+    int green_byte = int(255.999 * g);
+    int blue_byte = int(255.999 * b);
+
+    // write out the pixel color components
+    out << red_byte << ' ' << green_byte << ' ' << blue_byte << '\n';
 }
 
 #endif
